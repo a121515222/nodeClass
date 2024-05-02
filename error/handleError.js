1b00003456542326fa4c5d74124cfc3359e02c23
CHUN-CHIA <a121515222@gmail.com>
2024-05-03 01:59:51 +0800

    refactor handleError

--- error/handleError.js
+++ error/handleError.js
@@ -1,63 +1,44 @@
-const e = require("express");
-
 const handleProductionError = (err, res) => {
-  //mongoose錯誤
-  console.log("err.name", err.name);
-  if (err.name === "ValidationError") {
-    //取出mongoose給錯誤訊息
-    const message = Object.values(err.errors).map((val) => val.message);
-    res.status(err.statusCode || 500).json({ status: false, message: message });
-  } else if (err.name === "CastError") {
-    if (err.kind === "ObjectId") {
-      res.status(404).json({
-        status: false,
-        message: "找不到資料"
-      });
-    } else {
-      res.status(err.statusCode || 500).json({
-        status: false,
-        message: "資料庫忙碌中，請稍後再試"
-      });
-    }
-    //處理nodejs錯誤
-  } else if (err.name === "ReferenceError") {
-    res.status(err.statusCode || 500).json({
-      status: false,
-      message: "Server忙碌中，請稍後再試"
-    });
-    //處理自定義錯誤
-  } else if (err.name === "Error") {
-    res.status(err.statusCode || 500).json({
-      status: false,
-      message: err.message
-    });
+  let statusCode = err.statusCode || 500;
+  let message = "Internal Server Error";
+
+  switch (err.name) {
+    case "ValidationError":
+      message = Object.values(err.errors).map((val) => val.message);
+      break;
+    case "CastError":
+      message =
+        err.kind === "ObjectId" ? "找不到資料" : "資料庫忙碌中，請稍後再試";
+      statusCode = err.kind === "ObjectId" ? 404 : statusCode;
+      break;
+    case "ReferenceError":
+      message = "Server忙碌中，請稍後再試";
+      break;
+    case "Error":
+      message = err.message;
+      break;
+    default:
+      break;
   }
+
+  res.status(statusCode).json({ status: false, message });
 };
 
 const handleDevError = (err, res) => {
-  if (err.name === "ValidationError" || err.name === "CastError") {
-    res.status(err.statusCode || 500).json({
-      status: false,
-      message: {
-        errors: err.message,
-        name: err.name,
-        stack: err.stack
-      }
-    });
-  } else {
-    res.status(err.statusCode || 500).json({
-      status: false,
-      message: err
-    });
-  }
+  const statusCode = err.statusCode || 500;
+  const message = {
+    errors: err.message,
+    name: err.name,
+    stack: err.stack
+  };
+
+  res.status(statusCode).json({ status: false, message });
 };
 
-const customizeAppError = (httpStatus, errMessage, next) => {
+const customizeAppError = (httpStatus, errMessage) => {
   const error = new Error(errMessage);
   error.statusCode = httpStatus;
   error.isOperational = true;
   error.errorMessage = errMessage;
   return error;
 };
-
-module.exports = { handleProductionError, handleDevError, customizeAppError };
